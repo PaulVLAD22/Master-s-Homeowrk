117967167b644c03f72429b0a51904d007f4f5d5
vlad <vlad.munteanu@orange.com>
2022-10-14 20:38:48 +0300

    feat: initial form fields

--- Sem1/E-Government/HomeWork1/frontend/src/App.js
+++ Sem1/E-Government/HomeWork1/frontend/src/App.js
@@ -8,33 +8,20 @@ import {
   Code,
   Grid,
   theme,
+  Center,
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Logo } from './Logo';
+import Form from './components/Form';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
+      <Center width="100vw" height="100vh">
+        <Box textAlign="center" fontSize="xl" width="25%">
+          <Form />
+        </Box>
+      </Center>
     </ChakraProvider>
   );
 }
